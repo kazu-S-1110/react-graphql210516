eabae64320a27f30aef3eebb65bbeb517c3e224a
kazu-S-1110 <xtpgdmw3690@yahoo.co.jp>
2021-05-16 21:13:41 +0900

    環境構築とルートを作成

--- src/App.js
+++ src/App.js
@@ -1,24 +1,33 @@
-import logo from './logo.svg';
-import './App.css';
+import styles from './App.module.css';
+import React from 'react';
+import { ApolloProvider } from '@apollo/react-hooks';
+import { ApolloClient, InMemoryCache } from '@apollo/client';
+import { Route, BrowserRouter, Switch } from 'react-router-dom';
+import Auth from './components/Auth';
+import MainPage from './components/MainPage';
+
+const client = new ApolloClient({
+  uri: 'https://react-graphql210516.herokuapp.com/graphql/',
+  headers: {
+    authorization: localStorage.getItem('token')
+      ? `JWT ${localStorage.getItem('token')}`
+      : '',
+  },
+  cache: new InMemoryCache(), //apolloclientではクエリ結果をキャッシュに保存してくれる機能があるのでこのコードで保存してあげる
+});
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <ApolloProvider client={client}>
+      <div className={styles.app}>
+        <BrowserRouter>
+          <Switch>
+            <Route exact path="/" component={Auth} />
+            <Route exact path="/employees" component={MainPage} />
+          </Switch>
+        </BrowserRouter>
+      </div>
+    </ApolloProvider>
   );
 }
 
